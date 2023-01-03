1094ac7dbcd914ec7f50d0e641c8ae6108397816
VladimirGostik <xgostik@stuba.sk>
2023-01-03 17:49:57 +0100

    adam komitni si to pls

--- serviceworker.js
+++ serviceworker.js
@@ -1,28 +1,22 @@
-// const cache_container = "static_v2";
-// const files = [
-//     "/",
-//     "style.css",
-//     "java.js"
-// ]
+let deferredPrompt;
 
-self.addEventListener('instal', function (event){
-    // event.waitUntil(
-    //     caches.open(cache_container)
-    //         .then(cache => {
-    //             cache.addAll(files)
-    //         })
-    // )
+window.addEventListener('beforeinstallprompt', (e) =>{
+    e.preventDefault();
+    deferredPrompt = e;
+    btnAdd.style.display = 'block';
+});
+
+btnAdd.addEventListener('click', (e) => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if(choiceResult.outcome === 'accepted'){
+            console.log('User accepted the A2HS prompt');
+        }
+        deferredPrompt = null;
+    })
+});
+
+window.addEventListener('appinstalled', (evt) => {
+    app.logEvent('a2hs', 'installed');
 })
-self.addEventListener('activate',function (event){
-    console.log("service worker activated", event)
-})
-// self.addEventListener("fetch", function (event){
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(function (response){
-//                 if (response){
-//                     return response;
-//                 }
-//             })
-//     )
-// })
+
